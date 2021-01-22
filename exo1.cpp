19d252c6ae77af286843f81066b6fc9e09a3f470
Riad Mefti <riad.mefti@polymtl.ca>
2021-01-22 10:46:55 -0500

    a

---
+++ exo1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <string>
+#include<vector>
+using namespace std;
+
+
+
+
+int main()
+{
+	string phrase;
+	
+	vector<char> mot;
+
+	cout << "Saisissez une phrase :\n";
+	getline(cin, phrase);
+	
+	int i = 0;
+	int compteurLettre = 0;
+	int longueurPhrase = phrase.length();
+
+	/*for (int i = 0; i < longueurPhrase; i++)
+	{
+		if (phrase[i] != ' ')
+		{
+			compteurLettre++;
+			mot[i]+= phrase[i];
+			cout << phrase[i];
+			
+			
+		}
+		if (phrase[i] == ' ')
+		{
+			break;
+		}
+
+		cout << mot[0];
+		cout << mot[1];
+		
+	
+	}*/
+}
