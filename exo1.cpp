643bd10ee4254654a2120a4d87dd339391f2cef5
Riad Mefti <riad.mefti@polymtl.ca>
2021-01-22 12:32:48 -0500

    exo1

--- exo1.cpp
+++ exo1.cpp
@@ -1,42 +1,62 @@
 #include<iostream>
 #include <string>
-#include<vector>
 using namespace std;
 
 
+void trouverMots(string phrase)
+{
+    string mot;
+    string motCourt;
+    string motLong;
+    double compteurLettre = 0.0;
+    double compteurmots = 0.0;
+    int longueurPhrase = phrase.length();
+
+    for (int i = 0; i < longueurPhrase; i++)
+    {
+     
+        if (phrase[i] != ' ')
+        {
+            compteurLettre++;
+            mot += phrase[i];
+
+        }
+        if (phrase[i] == ' ' || i+1 == longueurPhrase)
+        {   
+            compteurmots++;
+            if (motCourt.length() == 0)
+            {
+                motCourt = mot;
+            }
+            if (mot.length() <= motCourt.length())
+            {
+                motCourt = mot;
+               
+            }
+
+            if (mot.length() >= motLong.length())
+            {
+                motLong = mot;
+
+            }
+
+            mot = "";
+        }
+
+    }
+
+    cout << "le mot le plus court est : "<<motCourt<<"\n";
+    cout << "le mot le plus Long est : " << motLong << "\n";
+    cout << "La longueur moyenne est : " << compteurLettre / compteurmots << " lettres\n";
+    
+}
 
 
 int main()
 {
     string phrase;
-
-    vector<char> mot;
-
     cout << "Saisissez une phrase :\n";
     getline(cin, phrase);
-
-    int i = 0;
-    int compteurLettre = 0;
-    int longueurPhrase = phrase.length();
-
-    / for (int i = 0; i < longueurPhrase; i++)
-    {
-        if (phrase[i] != ' ')
-        {
-            compteurLettre++;
-            mot[i] += phrase[i];
-            cout << phrase[i];
-
-
-        }
-        if (phrase[i] == ' ')
-        {
-            break;
-        }
-
-        cout << mot[0];
-        cout << mot[1];
-
-
-    } /
+    trouverMots(phrase);
+ 
 }
